c86c4e105313e5ccfcafc4a21f238bda224157fa
tamanna104 <94777585+tamanna104@users.noreply.github.com>
2022-06-14 22:46:52 +0600

    Delete dom.php

--- dom.php
+++
@@ -1,9 +0,0 @@
-<?php
-//session_start();
-$day = $_POST['day'];
-$month = $_POST['month'];
-$year = $_POST['year'];
-
-if($day == null || $month == null || $year == null ){
-    echo "Field is empty";
-}
