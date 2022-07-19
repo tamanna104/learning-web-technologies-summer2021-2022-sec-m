78bcd3be4a8c9c0583f43360ebefbf95fe19746a
tamanna104 <tamanna424101@gmail.com>
2022-07-19 16:57:15 +0600

    task1

---
+++ finalLabTask1/controller/login.php
@@ -0,0 +1,30 @@
+<?php 
+	session_start();
+
+	//print_r($_GET);  
+	$id = $_POST['id'];
+	$password = $_POST['password'];
+	//echo $username;
+
+
+	if($id == null || $password == null){
+		echo "null id/password";
+	}
+	else{
+		$file = fopen('../model/admin.txt', 'r');
+		
+		while (!feof($file)) {
+			$data=fgets($file);
+			$user = explode('|', $data);
+			if($id == trim($user[0]) && $password == trim($user[2])){
+				$_SESSION['status'] = true;
+				setcookie('status', 'true', time()+6000000, '/');
+				setcookie('myId', $id, time()+6000000, '/');
+				setcookie('myPassword', $password, time()+6000000, '/');
+				header('location: ../view/adminHome.php');
+			}
+		}
+		echo "invalid user <br><br>";
+		echo'<a href="../model/reg.html"> Back </a>';
+	}
+?>
